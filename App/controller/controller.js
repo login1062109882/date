0a362878f9f38a0a350af7f353c325196391efa5
along <along>
2018-01-01 20:14:18 +0800

    date

---
+++ App/controller/controller.js
@@ -0,0 +1,8 @@
+app.controller('myCtrl', function($scope,$rootScope,$http){
+    app.controller('myCtrl', function($scope, $interval) {
+  $scope.theTime = new Date().toLocaleTimeString();
+  $interval(function () {
+      $scope.theTime = new Date().toLocaleTimeString();
+  }, 1000);
+});
+})
